700ab2fa369b42b99c43ab64fcd70d98b29ca55c
Nguyen Le <ucnguyenle@gmail.com>
2017-04-18 17:44:10 -0700

    refactor: change in production code
    added an array of cats and made change to view model

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg',
+	    nicknames: ['D']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg',
+	    nicknames: ['n']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg',
+	    nicknames: ['A']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg',
+	    nicknames: ['B']
+        },
+        {
+            click : 0,
+            name : 'Sleepy',
+            imgSrc : 'https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg',
+	    nicknames: ['C']
+        }
+    ];
+
 var cats = function(data) {
 	this.click = ko.observable(data.click);
 	this.name = ko.observable(data.name);
@@ -24,12 +57,13 @@ var cats = function(data) {
 var viewModel = funtion(){
 	var self = this;
 	//store cat in current cat variable 
-	this.currentCat = ko.oberservable(new cats({
-	click: ko.observable(0),
-	name: ko.observable('Win'),
-	imgSrc: ko.observable('https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg'),
-  	nicknames: ko.observableArray(['A', 'B', 'C', 'D'])
-	}));
+	this.catList = ko.observableArray([]);
+	initialCats.foreach(function(catItem){
+		self.catList.push(new cats(catItem);
+	});
+	
+	this.currentCat = ko.oberservable(this.catList()[0]);
+	});
 	this.incrementCounter = function() {
 	self.currentCat().count(self.currentCat().count() + 1)
 };
